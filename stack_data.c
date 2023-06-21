0ba296e3aa900c0906ea0a95ffea83091b512681
Ahmed Mohamed <=>
2023-06-21 19:35:32 +0000

    fix pall

--- stack_data.c
+++ stack_data.c
@@ -81,10 +81,14 @@ void pall(stack_t **stack, unsigned int line_number)
 	(void) line_number;
 	if (*stack == NULL)
 		return;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
-		current = current->next;
+		current = current->prev;
 	}
 }
 
