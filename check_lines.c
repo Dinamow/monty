3f1879207ca4a9d6281e08665a5efa5d0c7140fb
DINAMOW <meemoo102039@gmail.com>
2023-06-22 13:13:18 +0000

    swap func

--- check_lines.c
+++ check_lines.c
@@ -9,9 +9,9 @@ void check_lines(char *lines)
 {
 	char *token, *token_2, *saveptr1, *temp;
 	stack_t *head = NULL;
-	unsigned int line_number = 1, i, arr_length = 4, no = 0;
+	unsigned int line_number = 1, i, arr_length = 5, no = 0;
 	instruction_t arr[] = {
-		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}
+		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}
 	};
 
 	token = strtok(lines, "\n");
--- pop.c
+++ pop.c
@@ -24,3 +24,27 @@ void pop(stack_t **stack, unsigned int line_number)
 		return;
 	current->prev = NULL;
 }
+/**
+ * swap - start point
+ * @stack: input
+ * @line_number: input
+ *
+ * Return: nothing
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+	stack_t *current = *stack;
+
+	if (*stack == NULL || current->next == NULL)
+	{
+		_free_stack(stack);
+		free(train.buff);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	current = current->next;
+	temp = current->n;
+	current->n = (*stack)->n;
+	(*stack)->n = temp;
+}
